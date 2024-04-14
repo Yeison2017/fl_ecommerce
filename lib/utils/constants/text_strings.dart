bfff1cef1ebce7ebbbac518037f7f777f56082ff
Yeison Hernandez <yha.big@gmail.com>
2024-04-14 17:26:54 -0500

    Se crea la pantalla para la crear una cuenta

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -33,6 +33,7 @@ class TTexts {
   static const String orSingUpWith = 'or sign up with';
   static const String iAgreeTo = 'I agree to';
   static const String privacyPolicy = 'Privacy Policy';
+  static const String and = 'and';
   static const String termsOfUse = 'Terms of use';
   static const String verifycationCode = 'verifycationCode';
   static const String resendEmail = 'Resend Email';
