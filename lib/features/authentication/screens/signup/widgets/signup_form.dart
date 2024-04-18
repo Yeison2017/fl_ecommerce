e506a2fe2a1e0270ea48f8eb585cc5e111f8c2fd
Yeison Hernandez <yha.big@gmail.com>
2024-04-17 21:59:39 -0500

    Pantalla de verificacion del email y cuenta creada

--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,7 +1,9 @@
+import 'package:fl_ecommerce/features/authentication/screens/signup/verify_email.dart';
 import 'package:fl_ecommerce/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart';
 import 'package:fl_ecommerce/utils/constants/sizes.dart';
 import 'package:fl_ecommerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class TSignupForm extends StatelessWidget {
@@ -78,7 +80,7 @@ class TSignupForm extends StatelessWidget {
         SizedBox(
           width: double.infinity,
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () => Get.to(() => const VerifyEmailScreen()),
             child: const Text(TTexts.createAccount),
           ),
         )
--- lib/utils/constants/image_string.dart
+++ lib/utils/constants/image_string.dart
@@ -9,18 +9,24 @@ class TImages {
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
-  // -- OnBoarding Images
-  // static const String onBoardingImage1 =
-  //     "assets/images/on_boarding_images/sammy-line-searching.gif";
-  // static const String onBoardingImage2 =
-  //     "assets/images/on_boarding_images/sammy-line-shopping.gif";
-  // static const String onBoardingImage3 =
-  //     "assets/images/on_boarding_images/sammy-line-delivery.gif";
-
   static const String onBoardingImage1 =
       "assets/images/on_boarding_images/sammy-line-searching.png";
   static const String onBoardingImage2 =
       "assets/images/on_boarding_images/sammy-line-shopping.png";
   static const String onBoardingImage3 =
       "assets/images/on_boarding_images/sammy-line-delivery.png";
+
+  // -- Animations
+  static const String productsIllustration =
+      'assets/images/animations/sammy-line-come-back-later.png';
+  static const String productSaleIllustration =
+      'assets/images/animations/sammy-line-sale.png';
+  static const String staticSuccessIllustration =
+      'assets/images/animations/sammy-line-success.png';
+  static const String deliveredInPlaneIllustration =
+      'assets/images/animations/sammy-line-come-back-later.png';
+  static const String deliveredEmailIllustration =
+      'assets/images/animations/sammy-line-man-receives-a-mail.png';
+  static const String verifyIllustration =
+      'assets/images/animations/sammy-line-travel-backpack-with-passport-and-air-ticket.png';
 }
