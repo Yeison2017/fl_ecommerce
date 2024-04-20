07f8283b441d2b2d7ac63dbeedc0b1f78cf70e37
Yeison Hernandez <yha.big@gmail.com>
2024-04-20 11:11:19 -0500

    Se crea el Bottom Navigation Bar

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:fl_ecommerce/features/authentication/password_configuration/forget_password.dart';
 import 'package:fl_ecommerce/features/authentication/screens/signup/signup.dart';
+import 'package:fl_ecommerce/navigation_menu.dart';
 import 'package:fl_ecommerce/utils/constants/sizes.dart';
 import 'package:fl_ecommerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
@@ -65,7 +66,7 @@ class TLoginForm extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () => Get.to(() => const NavigationMenu()),
               child: const Text(TTexts.signIn),
             ),
           ),
