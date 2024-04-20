99ce8a5ba458ba4dce639aad747b3c9f6f1e988b
Yeison Hernandez <yha.big@gmail.com>
2024-04-19 21:50:21 -0500

    Flutter forgot password UI

--- lib/utils/helpers/helper_funtions.dart
+++ lib/utils/helpers/helper_funtions.dart
@@ -5,21 +5,49 @@ import 'package:intl/intl.dart';
 class THelperFunctions {
   static Color? getColor(String value) {
     // Define your product specific colors here and it will match the attribute colors and show specific
-    if (value == 'Green') return Colors.green;
-    if (value == 'Red') return Colors.red;
-    if (value == 'Blue') return Colors.blue;
-    if (value == 'Pink') return Colors.pink;
-    if (value == 'Grey') return Colors.grey;
-    if (value == 'Purple') return Colors.purple;
-    if (value == 'Black') return Colors.black;
-    if (value == 'White') return Colors.white;
-    if (value == 'Yellow') return Colors.yellow;
-    if (value == 'Orange') return Colors.orange;
-    if (value == 'Brown') return Colors.brown;
-    if (value == 'Teal') return Colors.teal;
-    if (value == 'Indigo') return Colors.indigo;
+    // if (value == 'Green') return Colors.green;
+    // if (value == 'Red') return Colors.red;
+    // if (value == 'Blue') return Colors.blue;
+    // if (value == 'Pink') return Colors.pink;
+    // if (value == 'Grey') return Colors.grey;
+    // if (value == 'Purple') return Colors.purple;
+    // if (value == 'Black') return Colors.black;
+    // if (value == 'White') return Colors.white;
+    // if (value == 'Yellow') return Colors.yellow;
+    // if (value == 'Orange') return Colors.orange;
+    // if (value == 'Brown') return Colors.brown;
+    // if (value == 'Teal') return Colors.teal;
+    // if (value == 'Indigo') return Colors.indigo;
 
-    return null;
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if (value == 'Blue') {
+      return Colors.blue;
+    } else if (value == 'Pink') {
+      return Colors.pink;
+    } else if (value == 'Grey') {
+      return Colors.grey;
+    } else if (value == 'Purple') {
+      return Colors.purple;
+    } else if (value == 'Black') {
+      return Colors.black;
+    } else if (value == 'White') {
+      return Colors.white;
+    } else if (value == 'Yellow') {
+      return Colors.yellow;
+    } else if (value == 'Orange') {
+      return Colors.orange;
+    } else if (value == 'Brown') {
+      return Colors.brown;
+    } else if (value == 'Teal') {
+      return Colors.teal;
+    } else if (value == 'Indigo') {
+      return Colors.indigo;
+    } else {
+      return null;
+    }
   }
 
   static void showSnackBar(String message) {
