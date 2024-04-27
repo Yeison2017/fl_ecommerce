2f1e83589b6e8952ac3ac09d223afb3d5364e7b0
Yeison Hernandez <yha.big@gmail.com>
2024-04-27 11:59:09 -0500

    Custom AppBar Design

---
+++ lib/common/widgets/appbar/appbar.dart
@@ -0,0 +1,46 @@
+import 'package:fl_ecommerce/utils/constants/sizes.dart';
+import 'package:fl_ecommerce/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TAppBar({
+    super.key,
+    this.title,
+    this.actions,
+    this.leadingIcon,
+    this.leadingOnPressed,
+    this.showBackArray = false,
+  });
+
+  final Widget? title;
+  final bool showBackArray;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArray
+            ? IconButton(
+                onPressed: () => Get.back(),
+                icon: const Icon(Iconsax.arrow_left))
+            : leadingIcon != null
+                ? IconButton(
+                    onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                : null,
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
