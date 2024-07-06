2a5b4723257105b4f63283cdb43120e58d07148d
Yeison Hernandez <yha.big@gmail.com>
2024-07-06 12:17:28 -0500

    Product Detail page

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,39 +1,97 @@
+import 'package:fl_ecommerce/common/widgets/texts/section_heading.dart';
+import 'package:fl_ecommerce/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
+import 'package:fl_ecommerce/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:fl_ecommerce/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:fl_ecommerce/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:fl_ecommerce/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:fl_ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             // 1 - Product Image Slider
-            TProductImageSlider(),
+            const TProductImageSlider(),
 
             // 2 - Product Details
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                   right: TSizes.defaultSpace,
                   left: TSizes.defaultSpace,
                   bottom: TSizes.defaultSpace),
               child: Column(
                 children: [
                   // Rating & Share Button
-                  TRatingAndShare(),
+                  const TRatingAndShare(),
 
                   // Price, Title, Stock, & Brand
-                  TProductMetaData(),
+                  const TProductMetaData(),
 
                   // Attributes
+                  const TProductAttributes(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
                   // Checkout Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: const Text('Checkout')),
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
                   // Description
+                  const TSectionHeading(
+                    title: 'Description',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems,
+                  ),
+                  const ReadMoreText(
+                    'This is a Product description for Blue Nike Sleeve less vest. There are more things that can be added but i This is a Product description for Blue Nike Sleeve less vest. There are more things that can be added but i ',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: ' Show more',
+                    trimExpandedText: ' Less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
                   // Reviews
+                  const Divider(),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                        title: 'Reviews (199)',
+                        showActionButton: false,
+                      ),
+                      IconButton(
+                          icon: const Icon(
+                            Iconsax.arrow_right3,
+                            size: 18,
+                          ),
+                          onPressed: () {})
+                    ],
+                  )
                 ],
               ),
             )
